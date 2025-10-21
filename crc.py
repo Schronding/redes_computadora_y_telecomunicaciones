ebb44216d99a8516cf744eb0d61c77e890419e2f
schronding <schronding@gmail.com>
2025-10-21 09:44:58 -0600

    Codigo de redundancia ciclica (CRC) terminado

--- crc.py
+++ crc.py
@@ -1,14 +1,33 @@
-M_str = "11010"
-G_str = "1011"
-length_G = len(G_str)
+M_str = "1101011011"
+G_str = "10011"
 
-def codigo_rendundancia_ciclica(G, M):
-    result_str = ""
-    for index in range(length_G):
-        if M_str[index] == G_str[index]:
-            result_str += "0"
-        else:
-            result_str += "1"
-    return codigo_rendundancia_ciclica(10011, result_str + M_str[length_G:]) 
+def calcular_crc(msg_str, gen_str):
 
-print(codigo_rendundancia_ciclica(G_str, M_str)[:-(length_G-1)])
+    msg = [int(bit) for bit in msg_str]
+    gen = [int(bit) for bit in gen_str]
+    
+    k = len(gen)
+    padding_len = k - 1
+    
+    dividend = msg + [0] * padding_len
+    
+    for i in range(len(msg)):
+        
+        if dividend[i] == 1:
+            for j in range(k):
+                dividend[i+j] = (dividend[i+j] + gen[j]) % 2
+        
+        remainder = dividend[-(padding_len):]
+    
+    remainder_str = "".join(map(str, remainder))
+    
+    return remainder_str
+
+
+crc_remainder = calcular_crc(M_str, G_str)
+trama_transmitida = M_str + crc_remainder
+
+print(f"Mensaje Original (M):     {M_str}")
+print(f"Polinomio Generador (G):  {G_str}")
+print(f"Residuo CRC (R):          {crc_remainder}")
+print(f"Trama Transmitida (M + R):  {trama_transmitida}")
